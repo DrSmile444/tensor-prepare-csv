a0cd983a5720a873147f3a3be1a2b443e6624486
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-05-22 16:16:28 +0300

    chore(parse): optimize remove similar algo by using pm2 server

--- remove-similar.js
+++ remove-similar.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 
-const stringSimilarity = require('string-similarity');
+const axios = require('axios');
+const queue = require('queue');
 
 const dataset = require('./1-3-temp/tensor-csv-dataset.json');
 
-const newDataset = dataset.filter((item, index, self) => {
-  if (index % 100 === 0) {
-    console.info(index, 'of', dataset.length, ((index / dataset.length) * 100).toFixed(2), '%');
-  }
+(async () => {
+  const q = queue({ results: [], timeout: 200, concurrency: 100, autostart: true });
 
-  return self.findIndex((searchItem) => {
-    return stringSimilarity
-      .compareTwoStrings(searchItem.commenttext, item.commenttext) > 0.5
-  }) === index;
-});
+  dataset.forEach((item, index, self) => q.push(async () => {
+    if (index % 100 === 0) {
+      console.info(index, 'of', dataset.length, ((index / dataset.length) * 100).toFixed(2), '%');
+    }
 
-fs.writeFileSync('./1-3-temp/tensor-csv-dataset-result.json', JSON.stringify(newDataset, null ,2));
+    for (let i = 0; i < self.length; self++) {
+      const { isSame } = (await axios.post('http://localhost:3040/compare',{
+        first: item.commenttext,
+        second: self[i].commenttext,
+        rate: 0.3,
+      })).data;
+
+      if (isSame && i !== index) {
+        return {
+          item, unique: false,
+        }
+      }
+    }
+
+    return { item, unique: true };
+  }));
+
+  q.start(function (err) {
+    if (err) throw err
+    console.log('all done:', q.results);
+    fs.writeFileSync('./1-3-temp/tensor-csv-dataset-result-server.json', JSON.stringify(q.results, null ,2));
+  });
+})();
