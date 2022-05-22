92948fe87a4256693aeaf28b56e7aef5bed0d695
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-05-22 16:30:13 +0300

    chore(parse): use worker farm for heavy calculations

--- remove-similar.js
+++ remove-similar.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 const axios = require('axios');
 const queue = require('queue');
+const workerFarm = require('worker-farm');
+
+const workers = workerFarm(require.resolve('./remove-similar-logic.js'));
 
 const dataset = require('./1-3-temp/tensor-csv-dataset.json');
 
 (async () => {
-  const q = queue({ results: [], timeout: 200, concurrency: 100, autostart: true });
+  const q = queue({ results: [], timeout: 0, concurrency: 200, autostart: true });
 
   dataset.forEach((item, index, self) => q.push(async () => {
     if (index % 100 === 0) {
@@ -14,11 +17,14 @@ const dataset = require('./1-3-temp/tensor-csv-dataset.json');
     }
 
     for (let i = 0; i < self.length; self++) {
-      const { isSame } = (await axios.post('http://localhost:3040/compare',{
+      const compareOptions = {
         first: item.commenttext,
         second: self[i].commenttext,
         rate: 0.3,
-      })).data;
+      };
+      const { isSame } = await new Promise((resolve) => {
+        workers(compareOptions, resolve);
+      });
 
       if (isSame && i !== index) {
         return {
@@ -33,6 +39,9 @@ const dataset = require('./1-3-temp/tensor-csv-dataset.json');
   q.start(function (err) {
     if (err) throw err
     console.log('all done:', q.results);
+    workerFarm.end(workers);
     fs.writeFileSync('./1-3-temp/tensor-csv-dataset-result-server.json', JSON.stringify(q.results, null ,2));
+
+    console.log('File wrote');
   });
 })();
